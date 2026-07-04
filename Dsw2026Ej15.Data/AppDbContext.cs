ec9bcbc48b5a206898be2d6a1aa73c4fbbb940e2
GermanEscobedo <Lautaro.Escobedo@alu.frt.utn.edu.ar>
2026-07-03 23:12:52 -0300

    se realiza ejercicio 16

---
+++ Dsw2026Ej15.Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dsw2026Ej15.Domain; 
+
+namespace Dsw2026Ej15.Data
+{
+    public class AppDbContext : DbContext
+    {
+        
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                
+                optionsBuilder.UseSqlServer("Server=localhost;Database=DSW2026_Ej16;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+}
---
+++ Dsw2026Ej15.Data/PersistenceEf.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dsw2026Ej15.Domain;
+
+namespace Dsw2026Ej15.Data
+{
+    public class PersistenceEf : IPersistence
+    {
+        private readonly AppDbContext _context;
+
+        public PersistenceEf()
+        {
+            _context = new AppDbContext();
+        }
+
+        
+        public IEnumerable<Speciality> GetAllSpecialities()
+        {
+            return _context.Specialities.ToList();
+        }
+
+        
+        public IEnumerable<Doctor> GetActiveDoctors()
+        {
+           
+            return _context.Doctors.Where(d => d.IsActive).ToList();
+        }
+
+        
+        public Doctor GetActiveDoctorById(Guid id)
+        {
+            return _context.Doctors.FirstOrDefault(d => d.Id == id && d.IsActive);
+        }
+
+       
+        public bool DeactivateDoctor(Guid id)
+        {
+            var doctor = _context.Doctors.FirstOrDefault(d => d.Id == id);
+
+            if (doctor != null)
+            {
+                doctor.IsActive = false;
+
+                _context.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
